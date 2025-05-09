0a384c81f1d711a063d6de8a7b6bd0d7bf2c4438
Jessika Karlsson <jessikakarlsson@MacBook-Pro-som-tillhor-Jessika.local>
2025-05-09 23:34:19 +0200

    product page

--- pages/home.php
+++ pages/home.php
@@ -17,20 +17,15 @@ global $dbContext, $cart;
         <meta name="description" content="" />
         <meta name="author" content="" />
         <title>Shop Homepage</title>
-        <!-- Favicon-->
         <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
-        <!-- Bootstrap icons-->
         <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css" rel="stylesheet" />
-        <!-- Core theme CSS (includes Bootstrap)-->
         <link href="/css/styles.css" rel="stylesheet" />
         <link href="/css/custom.css" rel="stylesheet" />
 
     </head>
     <body>
-        <!-- Navigation-->
         <?php echo HeaderNav($dbContext, $cart) ?>
 
-        <!-- Header-->
         <header>
             <div class="hero">
                 <img src="./assets/hero.jpg" alt="hero image">
@@ -41,7 +36,6 @@ global $dbContext, $cart;
             </div>
             </div>
         </header>
-        <!-- Section-->
         <section class="py-5">
             <div class="container px-4 px-lg-5 mt-5">
                 <div class="row gx-4 gx-lg-5 row-cols-2 row-cols-md-3 row-cols-xl-4 justify-content-center">
@@ -53,20 +47,15 @@ global $dbContext, $cart;
                                 <?php if($prod->price < 10) {  ?>
                                     <div class="badge bg-dark text-white position-absolute" style="top: 0.5rem; right: 0.5rem">Sale</div>
                                 <?php } ?>        
-                                <!-- Product image-->
-                                 <!--<a href="/productPage?id=<?php echo $prod->id; ?>">-->
+                                <a href="/productPage?id=<?php echo $prod->id; ?>">
                                     <img src="<?php echo $prod->imageUrl ?>" alt="<?php echo $prod->title ?>">
-                                 <!--</a>-->
-                                <!-- Product details-->
+                                </a>
                                 <div class="card-body p-4">
                                     <div class="text-center">
-                                        <!-- Product name-->
                                         <h5 class="fw-bolder"><?php echo $prod->title; ?></h5>
-                                       <!-- Product price-->
                                         $<?php echo $prod->price; ?>.00
                                     </div>
                                 </div>
-                                <!-- Product actions-->
                                 <div class="card-footer p-4 pt-0 border-top-0 bg-transparent">
                                     <div class="text-center"><a class="btn btn-outline-dark mt-auto" href="/addToCart?productId=<?php echo $prod->id ?>&fromPage=<?php echo urlencode((empty($_SERVER['HTTPS']) ? 'http' : 'https') . "://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]" ) ?>">Add to cart</a></div>
                                 </div>
@@ -76,11 +65,8 @@ global $dbContext, $cart;
                 </div>
             </div> 
         </section>
-        <!-- Footer-->
          <?php Footer(); ?>
-        <!-- Bootstrap core JS-->
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
-        <!-- Core theme JS-->
         <script src="js/scripts.js"></script>
 
 
--- pages/productPage.php
+++ pages/productPage.php
@@ -10,15 +10,6 @@ $id = $_GET['id'];
 
 $product = $dbContext->getProduct($id);
 
-
-if ($_SERVER['REQUEST_METHOD'] == 'POST'){
-    $product->title = $_POST['title'];
-    $product->stockLevel = $_POST['stockLevel'];
-    $product->price = $_POST['price'];
-    $product->imageUrl = $_POST['imageUrl'];
-}else{
-}
-
 ?>
 
 <!DOCTYPE html>
@@ -32,36 +23,36 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST'){
         <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
         <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css" rel="stylesheet" />
         <link href="/css/styles.css" rel="stylesheet" />
+        <link href="/css/custom.css" rel="stylesheet" />
+
     </head>
     <body>
         <?php echo HeaderNav($dbContext, $cart) ?>
 
         <section class="py-5">
-        <div class="container px-4 px-lg-5 mt-5">
-            <?php foreach($dbContext->getProduct($id) as $prod){ ?>
-                <div>
-                    <?php echo $prod->imageUrl; ?> 
+            <div class="container px-4 px-lg-5 mt-5">
+                <div class="row">
+                    <div class="col mb-5">
+                        <img class="img-fluid" src="<?php echo $product->imageUrl ?>" alt="<?php echo $product->title ?>">
+                    </div>
+                    <div class="col mb-5">
+                        <h1><?php echo $product->title ?></h1>
+                        <h3>Price: $<?php echo $product->price; ?></h3>
+                        <p><strong>Stock level:</strong><?php echo $product->stockLevel; ?></p>
+                        <p><strong>Description:</strong>Beautiful poster for your home!</p>
+
+                    </div>
+
                 </div>
-                <div>
-                    <h1><?php echo $prod->title; ?></h1> 
-                </div>
-                <div>
-                    <p>description </p>
-                </div>
-                <div>
-                    <h2><?php echo $prod->price; ?></h2> 
-                </div>
-                <div>
-                    <h2><?php echo $prod->stockLevel; ?></h2> 
-                </div>
+
                 <div class="card-footer p-4 pt-0 border-top-0 bg-transparent">
                     <div class="text-center">
                         <a class="btn btn-outline-dark mt-auto" href="/addToCart?productId=<?php echo $prod->id ?>
                         &fromPage=<?php echo urlencode((empty($_SERVER['HTTPS']) ? 'http' : 'https') .
-                        "://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]" ) ?>">Add to cart</a></div>
+                        "://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]" ) ?>">Add to cart</a>
+                    </div>
                 </div>            
-            <?php } ?>
-        </div>
+            </div>
         </section>
         <?php Footer(); ?>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
